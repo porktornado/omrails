0323f736e2d0c9c8bfe537e8c48f1f02783faf38
Steve Medina <porktornado@gmail.com>
2013-08-18 14:16:39 -0700

    Adding CSS to pins

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :name, :first_name, :last_name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
   has_many :pins
 end
