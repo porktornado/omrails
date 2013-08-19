2f0ecd893fe3854e9278279296b1148d53b78ca1
Steve Medina <porktornado@gmail.com>
2013-08-18 18:24:14 -0700

    Connect to Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Omrails::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
     # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # config/environments/production.rb
+  #configuring for S3 & paperclip uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+}
 end
